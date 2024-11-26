33d1f8ae802a9c88db1bd2c49351991ea5862b6b
Daiane Darosci <167923148+darosci@users.noreply.github.com>
2024-11-25 21:26:36 -0300

    Terceiro commit agora do github para eclipse

--- moreDevsAulaGit/src/moreDevsAulaGit/ExemploIntegracaoGit.java
+++ moreDevsAulaGit/src/moreDevsAulaGit/ExemploIntegracaoGit.java
@@ -5,6 +5,7 @@ public class ExemploIntegracaoGit {
 	public static void main(String[] args) {
 		System.out.println("Hello Word");
 		System.out.println("Ciao Mondo");
+		System.out.println("Olá Mundo");
 	}
 
 }
